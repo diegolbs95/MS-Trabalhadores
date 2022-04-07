3ab27983f76f6509d919225323c132b46306cb2d
Diego Luis <diego_bezerra95@hotmail.com>
2022-04-07 00:34:52 -0300

    MS hr-folhapagamento criado

---
+++ hr-folhapagamento/src/main/java/com/pg/hrfolhapagamento/HrFolhapagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.pg.hrfolhapagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrFolhapagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrFolhapagamentoApplication.class, args);
+	}
+
+}
---
+++ hr-folhapagamento/src/test/java/com/pg/hrfolhapagamento/HrFolhapagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.hrfolhapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrFolhapagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
