2ae4b479067c66dbd9d608a1af8b738c1d00941d
Diego Luis <diego_bezerra95@hotmail.com>
2022-04-06 22:40:53 -0300

    Projeto criado

---
+++ hora-trabalhador/src/main/java/com/horatrabalhador/HoraTrabalhadorApplication.java
@@ -0,0 +1,13 @@
+package com.horatrabalhador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraTrabalhadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraTrabalhadorApplication.class, args);
+	}
+
+}
---
+++ hora-trabalhador/src/test/java/com/horatrabalhador/HoraTrabalhadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horatrabalhador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraTrabalhadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
